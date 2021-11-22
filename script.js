056849b86b13e7661be79b0bf7e3bbdd289dbc38
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2021-11-22 09:39:46 +0000

    gets current data from api and stores values in local variables

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 // Current weather app
+//IMPORTANT DOCS ===============
+// https://openweathermap.org/weather-data
+
 const apiKey = "8fcf15f1446775617fe9577e790f0250";
 const pathName = "https://api.openweathermap.org/data/2.5/";
 // const url =
@@ -7,13 +10,21 @@ const pathName = "https://api.openweathermap.org/data/2.5/";
 const forecastUrl = "forecast?q=";
 const currentUrl = "weather?q=";
 
+// https://openweathermap.org/current
+const unitsImperial = "&units=imperial";
+
 let variable = [];
 let lon;
 let lat;
 let searchHistory = [];
+let currentWind;
+let currentHumidity;
+let cityName;
+let currentDate;
+let currentTemp;
 
 var getUserRepos = function (city, typeofUrl) {
-  var apiUrl = pathName + typeofUrl + city + "&appid=" + apiKey;
+  var apiUrl = pathName + typeofUrl + city + unitsImperial + "&appid=" + apiKey;
 
   fetch(apiUrl)
     .then(function (response) {
@@ -24,6 +35,12 @@ var getUserRepos = function (city, typeofUrl) {
           variable = data;
           lon = variable.coord.lon;
           lat = variable.coord.lat;
+          currentWind = variable.wind.speed + " MPH";
+          currentHumidity = variable.main.humidity + " %";
+          cityName = variable.name;
+          currentDate = variable.dt;
+          // need to add small upper circle for F
+          currentTemp = variable.main.temp + "°F";
         });
       } else {
         alert("Error: " + response.statusText);
