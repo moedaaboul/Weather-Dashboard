1aac76f155b9d77b5fdfd783dd498793eac871e6
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2021-11-28 22:41:23 +0000

    restricts uvi colour scales to three

--- global.js
+++ global.js
@@ -1,15 +1,11 @@
 export const colorUvi = (uvi) => {
   if (uvi < 3) {
     document.querySelector(".uvi-color").style.backgroundColor = "green";
-  } else if (uvi >= 3 && uvi < 6) {
+  } else if (uvi >= 3 && uvi < 8) {
     document.querySelector(".uvi-color").style.backgroundColor = "yellow";
     document.querySelector(".uvi-color").style.color = "black";
-  } else if (uvi >= 6 && uvi < 8) {
-    document.querySelector(".uvi-color").style.backgroundColor = "brown";
-  } else if (uvi >= 8 && uvi < 11) {
-    document.querySelector(".uvi-color").style.backgroundColor = "red";
   } else {
-    document.querySelector(".uvi-color").style.backgroundColor = "#8b0000";
+    document.querySelector(".uvi-color").style.backgroundColor = "red";
   }
 };
 
